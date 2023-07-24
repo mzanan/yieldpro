3c46eaad5079ff615c085688426575bf9de050ab
Matias Zanan <matiaszanan@gmail.com>
2023-07-24 12:01:05 -0300

    Header mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,7 @@ module.exports = {
         surface: "#FEFEFE",
         primary: "#0E2549",
         secondary: "#66CCCC",
-        accent: "#FF6600",
-        body: "#3F3F3F",
-        comment: "#959595",
-        smokeBlack: "#1E1E1E",
-        inputs: "#F1F1F1",
+        white: "#F8F7F7",
       },
       backgroundImage: {
         "header-background": "url('/assets/images/header-background.png')",
