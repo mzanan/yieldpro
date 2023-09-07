27864301797cb9137ab8b549e8143282d3df5487
Matias Zanan <matiaszanan@gmail.com>
2023-09-07 19:08:37 -0300

    [Collapsible Menu]

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,12 @@ module.exports = {
       colors: {
         surface: "#FEFEFE",
         primary: "#6A2C93",
+        secondary: "#B1AFB4",
         white: "#F8F7F7",
         gray: "#646464",
         black: "#0C0C0C",
+        hover: "#F1ECF7",
+        active: "#E8DEF8",
       },
     },
   },
